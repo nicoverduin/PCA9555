c11188962dbdffc0bb6ecfcacafbc9a80f7ebc90
Jared Reabow <ox141jf@hotmail.co.uk>
2023-04-16 16:27:47 +1000

    added support for dual I2c

--- clsPCA9555.h
+++ clsPCA9555.h
@@ -3,6 +3,8 @@
  *
  *  Created on: 27 jul. 2015
  *      Author: Nico
+ *
+ * Modified by Jared Reabow to include support for dual I2C devices (16/04/2023)
  */
 
 #ifndef CLSPCA9555_H_
@@ -38,7 +40,7 @@ enum {
 
 class PCA9555 {
 public:
-    PCA9555(uint8_t address, int interruptPin = -1);     // optional interrupt pin in second argument
+    PCA9555(uint8_t address,uint8_t IO_SDA, uint8_t IO_SCL, int interruptPin = -1);     // optional interrupt pin in second argument
     void pinMode(uint8_t pin, uint8_t IOMode );          // pinMode
     uint8_t digitalRead(uint8_t pin);                    // digitalRead
     void digitalWrite(uint8_t pin, uint8_t value );      // digitalWrite
